731f1bed5a54c239d91998aefd68ead0e6483507
cho881020 <kj_cho@idealidea.co.kr>
2020-06-06 14:36:19 +0900

    연결버튼 누르면 전화가 걸리도록  => Fragment에서 액티비티의 코드를 짜듯이 표현

--- app/src/main/java/kr/co/tjoeun/fragmenttest_20200606/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragmenttest_20200606/fragments/MyProfileFragment.kt
@@ -1,10 +1,14 @@
 package kr.co.tjoeun.fragmenttest_20200606.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 import kr.co.tjoeun.fragmenttest_20200606.R
 
 class MyProfileFragment : Fragment() {
@@ -19,6 +23,14 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        dialBtn.setOnClickListener {
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+            val myUri = Uri.parse("tel:$inputPhoneNum")
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+        }
     }
 
 }
