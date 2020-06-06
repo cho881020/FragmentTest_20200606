9dc692205faf8fe8f1ff2f92b9af753740313877
cho881020 <kj_cho@idealidea.co.kr>
2020-06-06 14:12:33 +0900

    두개의 프래그먼트가 페이지의 내용물로 등장

--- app/src/main/java/kr/co/tjoeun/fragmenttest_20200606/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/fragmenttest_20200606/MainActivity.kt
@@ -2,8 +2,12 @@ package kr.co.tjoeun.fragmenttest_20200606
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.fragmenttest_20200606.adapters.MainViewPagerAdapter
 
 class MainActivity : BaseActivity() {
+
+    lateinit var mainAdapter : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        mainAdapter = MainViewPagerAdapter(supportFragmentManager)
+        myViewPager.adapter = mainAdapter
     }
 
 }
