d047c2c7665fa591ffdf160eda4c1f8ecb19ff29
cho881020 <kj_cho@idealidea.co.kr>
2020-06-06 14:05:42 +0900

    일단 배경색만 다르게 두개의 Fragment 생성

---
+++ app/src/main/java/kr/co/tjoeun/fragmenttest_20200606/fragments/MyProfileFragment.kt
@@ -0,0 +1,24 @@
+package kr.co.tjoeun.fragmenttest_20200606.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.fragmenttest_20200606.R
+
+class MyProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
